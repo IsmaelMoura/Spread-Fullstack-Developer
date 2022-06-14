e0781ee46d6bcb34baf3465c5bb2a6a56f4a5cff
Ismael Moura <ismaeldesousa2014@outlook.com>
2022-06-14 18:59:18 -0300

    adiciona exercicios de modulos

---
+++ projetos/modules/main.js
@@ -0,0 +1,5 @@
+import { mostrarIdade, mostrarCidade, mostrarHobby } from './functions.js';
+
+console.log(mostrarIdade('Ismael', 18));
+console.log(mostrarCidade('Ismael', 'São Paulo'));
+console.log(mostrarHobby('Ismael', 'Estudar programação'));
